f3e30d47a866fe3c234e03230e73f5cc0afb0d31
Lizhe <Lizhe@172.21.12.235>
2017-06-30 16:40:58 +0800

    init

---
+++ src/main/java/websocketsample/MyWebSocket.java
@@ -0,0 +1,78 @@
+package websocketsample;
+
+import java.io.IOException;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.springframework.stereotype.Component;
+
+@ServerEndpoint(value = "/websocket")
+@Component
+public class MyWebSocket {
+
+    //concurrent包的线程安全Set，用来存放每个客户端对应的MyWebSocket对象。
+    private static CopyOnWriteArraySet<MyWebSocket> webSocketSet = new CopyOnWriteArraySet<MyWebSocket>();
+
+    //与某个客户端的连接会话，需要通过它来给客户端发送数据
+    private Session session;
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(this);     //加入set中
+        try {
+            sendMessage("opened");
+        } catch (IOException e) {
+            System.out.println("IO exception");
+        }
+    }
+
+    @OnClose
+    public void onClose() {
+        webSocketSet.remove(this);
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        System.out.println("received message :" + message);
+
+        //群发消息
+        for (MyWebSocket item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("error");
+        error.printStackTrace();
+    }
+
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+        //this.session.getAsyncRemote().sendText(message);
+    }
+
+    public static void sendInfo(String message) throws IOException {
+        for (MyWebSocket item : webSocketSet) {
+            try {
+                item.sendMessage(message);
+            } catch (IOException e) {
+                continue;
+            }
+        }
+    }
+
+
+}
